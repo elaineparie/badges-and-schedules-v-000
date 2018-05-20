83f649dddca2e982a50f240fcfbe7e614e1365ec
Elaine Anderson <eparie@bu.edu>
2018-05-20 21:49:52 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,7 @@ def batch_badge_creator(names)
 end
 
 def assign_rooms(names, room)
-  names.collect do |name|
+  names.collect do |name, room|
   "Hello, #{name}! You'll be assigned to room #{room}!"
 end
 end
