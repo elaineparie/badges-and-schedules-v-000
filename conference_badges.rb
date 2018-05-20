7e3e86bb58e00edcdac283e7de5e3606ea148cc3
Elaine Anderson <eparie@bu.edu>
2018-05-20 21:36:12 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,3 +4,11 @@
 def badge_maker(name)
     "Hello, my name is #{name}."
   end
+
+names = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
+
+def batch_badge_creator(names)
+  names.each do |name|
+    "Hello, my name is #{name}."
+  end
+end
